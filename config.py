042abbcd13d6c93a73f9752470e3665f68ea7c34
Blkv_Andrey <59824211+2WelcomeHome1@users.noreply.github.com>
2023-09-24 14:52:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pubnub.pnconfiguration import PNConfiguration
 class PubNub_Connection(PNConfiguration):
     def __init__(self):
         super().__init__()
-        self.subscribe_key = "sub-c-f7df9fe1-d873-4c52-b6d1-d09161021e5d"
-        self.publish_key = "pub-c-8488393d-d966-4f8a-bbe9-3ec83cbb4a2f"
+        self.subscribe_key = "#"
+        self.publish_key = "# "
         self.user_id = "111"
         self.ssl = False
 
